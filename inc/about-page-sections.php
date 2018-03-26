ebdc1607dfa3f9d03776c9e557daa85d07918210
moxdev <moxdev1@gmail.com>
2018-03-26 11:31:25 -0400

    working on pop up modal

--- inc/about-page-sections.php
+++ inc/about-page-sections.php
@@ -7,9 +7,9 @@
 
 function fuse_eng_about_page_sections() {
 	if ( function_exists( 'get_field' ) ) {
-    if( have_rows( 'about_page_content_sections' ) ): ?>
+    if( have_rows( 'about_page_content_sections' ) ):
 
-      <?php while( have_rows('about_page_content_sections') ): the_row();
+      while( have_rows('about_page_content_sections') ): the_row();
         $top_places_to_work_section = get_sub_field( 'top_places_to_work_section' );
         $learn_more_video_section   = get_sub_field( 'learn_more_video_section' );
 
@@ -25,18 +25,27 @@ function fuse_eng_about_page_sections() {
 
         <?php endif;
 
-        if ( $top_places_to_work_section ) : ?>
+        if ( $top_places_to_work_section ) :
 
-        <section class="about-top-place-to-work">
-          <div class="wrapper">
-            <div class="content-wrapper">
-              <?php echo wp_kses_post( $top_places_to_work_section ); ?>
-            </div>
-          </div><!-- wrapper -->
-        </section><!-- about-top-place-to-work -->
+          while( have_rows('top_places_to_work_section') ): the_row();
+            $bkg_image = get_sub_field('top_place_to_work_background_image');
+            $content   = get_sub_field('top_place_to_work_content'); ?>
 
-        <?php endif;
+            <section class="about-top-place-to-work">
+              <figure class="bkg-image">
+                <img src="<?php echo esc_url( $bkg_image['url'] ); ?>" alt="<?php echo esc_attr( $bkg_image['alt'] ); ?>" description="<?php echo esc_attr( $bkg_image['description'] ); ?>">
+              </figure>
 
+              <div class="wrapper">
+                <div class="content-wrapper">
+                  <?php echo wp_kses_post( $content ); ?>
+                </div>
+              </div><!-- wrapper -->
+            </section><!-- about-top-place-to-work -->
+
+          <?php endwhile;
+
+        endif;
 
       endwhile;
 
