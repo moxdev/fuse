ecc0387853663803eaaa832cd81f58e3266c2b36
moxdev <moxdev1@gmail.com>
2018-03-26 10:12:37 -0400

    bkgd images for section

--- inc/about-page-sections.php
+++ inc/about-page-sections.php
@@ -13,18 +13,6 @@ function fuse_eng_about_page_sections() {
         $top_places_to_work_section = get_sub_field( 'top_places_to_work_section' );
         $learn_more_video_section   = get_sub_field( 'learn_more_video_section' );
 
-        if ( $top_places_to_work_section ) : ?>
-
-        <section class="about-top-place-to-work">
-          <div class="wrapper">
-            <div class="content-wrapper">
-              <?php echo wp_kses_post( $top_places_to_work_section ); ?>
-            </div>
-          </div><!-- wrapper -->
-        </section><!-- about-top-place-to-work -->
-
-        <?php endif;
-
         if ( $learn_more_video_section ) : ?>
 
         <section class="about-learn-more-video">
@@ -34,6 +22,18 @@ function fuse_eng_about_page_sections() {
             </div>
           </div><!-- wrapper -->
         </section><!-- about-learn-more-video -->
+
+        <?php endif;
+
+        if ( $top_places_to_work_section ) : ?>
+
+        <section class="about-top-place-to-work">
+          <div class="wrapper">
+            <div class="content-wrapper">
+              <?php echo wp_kses_post( $top_places_to_work_section ); ?>
+            </div>
+          </div><!-- wrapper -->
+        </section><!-- about-top-place-to-work -->
 
         <?php endif;
 
